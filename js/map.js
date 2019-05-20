64c8a2aef78dff3129fe4fd8d4e20c83668ac9e0
Connor McCaffrey <connor.mccaffrey@gmail.com>
2019-05-20 15:37:45 +1000

    Added cart. Improved pages, responsive design, styles & map

--- js/map.js
+++ js/map.js
@@ -1,9 +1,8 @@
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
+    var location = {lat: -27.4493499, lng: 152.9777008};
+
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+        document.getElementById('map'), {zoom: 18, center: location});
+
+    var marker = new google.maps.Marker({position: location, map: map});
 }  
